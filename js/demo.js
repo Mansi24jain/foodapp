e42315139729824b38eb04a559d598e0f1ca17fe
mansi <mansijain2405@gmail.com>
2019-07-23 10:51:53 +0530

    updated project

---
+++ js/demo.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+// 	$('h1').click(function() {
+// 		$('h1').css('background-color', 'yellow');
+// 	});
+// });
+$(document).ready(function() {
+	$('nav').click(function() {
+		$('nav').addClass('sticky');
+	});
+	// $('h1').click(function() {
+	// 	$('h1').addClass('jquery');
+	// });
+	// $('nav').click(function() {
+	// 	$('#features').show("animated","fadeInUP");
+	// });
+});
